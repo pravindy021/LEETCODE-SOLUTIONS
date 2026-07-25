c8a01fa3327d9e203b4db28faea63d8189b552e7
asy <pravindyadav272@gmail.com>
2026-07-25 23:30:45 +0530

    [3536] [Time Beats: 100.00%] [Memory Beats: 22.61%] - LeetPush

---
+++ 3536-Maximum-Product-of-Two-Digits.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, n: int) -> int:
+        first = second = 0
+
+        while n:
+            d = n % 10
+            n //= 10
+
+            if d >= first:
+                second = first
+                first = d
+            elif d > second:
+                second = d
+
+        return first * second
+        
